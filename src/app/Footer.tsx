2833329dcdbd3de862ea38df53879783d4bbba2d
johnnysn <johnnysn@gmail.com>
2023-10-30 09:30:06 -0300

    feat: Initial layout

---
+++ src/app/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="flex justify-center">
+      <div className="max-w-screen-xl flex items-center"> &copy; Copyright 2023 </div>
+    </footer>
+  );
+}
---
+++ src/app/Header.tsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <header className="flex justify-center bg-cyan-500 text-gray-50 border-b-8 border-b-cyan-600">
+      <nav className="max-w-screen-xl w-full px-2 md:px-6 flex items-center justify-between">
+        <div>Logo</div>
+
+        <ul>Menu</ul>
+      </nav>
+    </header>
+  );
+}
