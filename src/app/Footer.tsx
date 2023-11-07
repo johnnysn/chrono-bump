c138402fc2592d5044be0bd1d5bc63957442cdb0
Johnny <johnny.gomes@ufjf.br>
2023-11-07 11:29:23 -0300

    feat: Update the footer information about the project

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -1,8 +1,10 @@
 export default function Footer() {
   return (
     <footer className="flex justify-center">
-      <div className="max-w-screen-xl flex items-center">
-        Developed by Johnny Gomes - &copy; Copyright 2023
+      <div className="max-w-screen-xl text-center mt-2">
+        <span className="text-sm">Open Source Project - Released under the MIT License</span>
+        <br />
+        &copy; Copyright 2023 by Johnny Gomes
       </div>
     </footer>
   );
